c95df33d46a7f197b78437f4d231d3c92a046c0b
Grayson Nocera <gnocera@purdue.edu>
2024-01-17 21:28:38 -0500

    add hw03 part 2

---
+++ HW03-2/filechar.c
@@ -0,0 +1,17 @@
+// ***
+// *** You MUST modify this file
+// ***
+
+#include <stdio.h>
+#include <stdbool.h>
+
+void printCounts(int * counts, int size)
+{
+  // print the values in counts in the following format
+  // each line has three items:
+  // ind, onechar, counts[ind]
+  // ind is between 0 and size - 1 (inclusive)
+  // onechar is printed if ind is between 'a' and 'z' or
+  // 'A' and 'Z'. Otherwise, print space
+  // if counts[ind] is zero, do not print
+}
